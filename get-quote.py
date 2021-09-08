aed1b226523d0435ff61be5461b040c77a6b75b7
virginiacattaneo <virginiacattaneo80@gmail.com>
2021-09-08 20:01:38 -0300

    listo el paso 7.agregue mas lineas de codigo al ejercicio

--- get-quote.py
+++ get-quote.py
@@ -9,6 +9,10 @@ def primary():
   f.close()
 
   print(quotes[rnd])
-
+  print(quotes[3])
+  print ("prueba python")
+  width = 20
+  height = 5 * 9
+  print (width * height)
 if __name__== "__main__":
   primary()
